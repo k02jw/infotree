f6b635cfd920a880c1c7414029d4c99ad3c6bff1
k02jw <sean94312941@gmail.com>
2025-05-29 19:53:33 +0900

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import { saveAllPosts } from './pgdb.js';
 import { crawl_club } from './campus_club.js';
 import { crawl_activity } from './campus_activity.js';
 import { crawl_contest } from './campus_contest.js';
+import { crawl_dgujanghak } from './dgujanghak.js';
+import { crawl_dgunotice } from './dgunotice.js';
 import { krcrawl } from './koreasch.js';
 
 export async function crawl() {
   const number = 5; //크롤링 문서 개수
-
+  const pagenum = 2; //동국대 홈페이지에서 크롤링할 페이지 수
+  
   const clubresult = await crawl_club(number);
   await saveAllPosts(clubresult);
   //캠퍼스픽_동아리
@@ -20,6 +23,14 @@ export async function crawl() {
   await saveAllPosts(contestresult);
   //캠퍼스픽_공모전
 
+  const dgujanghak = await crawl_dgujanghak(pagenum);
+  await saveAllPosts(dgujanghak);
+  //동국대학교 장학공지  
+
+  const dgugen = await crawl_dgunotice(pagenum);
+  await saveAllPosts(dgugen);
+  //동국대학교 일반공지  
+  
   const linkresult = await fetchsite();
   await saveAllPosts(linkresult);
   //링커리어
